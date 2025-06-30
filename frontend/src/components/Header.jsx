20617e28fac2ec52b817f9b7fa4907c95d8e2f4c
Pratik-dhangar <dhangarpratik5077@gmail.com>
2025-06-30 18:05:58 +0530

    day3

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const Header = () => {
+const {userData} = useContext(AppContext)
+
   return (
     <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800">
       <img
@@ -10,15 +13,15 @@ const Header = () => {
         className="w-36 h-36 rounded-full mb-6"
       />
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
-        Hey Developer{" "}
+        Hey {userData ? userData.name : 'Developer'}!
         <img src={assets.hand_wave} alt="" className="w-8 aspect-square" />
       </h1>
-      <h2>Welcome To Our App</h2>
-      <p>
+      <h2 className="text:3xl sm:text-5xl font-semibold mb-4">Welcome To Our App</h2>
+      <p className="mb-8 max-w-md">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi aliquid
         doloremque sapiente tempore porro consequuntur et est, temporibus.
       </p>
-      <button>Get Started</button>
+      <button className="border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all">Get Started</button>
     </div>
   );
 };
